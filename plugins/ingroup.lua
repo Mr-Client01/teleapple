b472dda9c97fb649a31fe81de4d9a7c938f34683
Mr Client <arashnomiri@gmail.com>
2016-05-17 22:59:03 +0000

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -204,7 +204,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "⚙Group settings:⚙\n>Lock group name : "..settings.lock_name.."\n>Lock group photo : "..settings.lock_photo.."\n>Lock group member : "..settings.lock_member.."\n>Lock group leave : "..leave_ban.."\n>flood sensitivity : "..NUM_MSG_MAX.."\n>Bot protection : "..bots_protection.."\n>Public: "..public.."\n">AntiSpam: ban\n>Group model: normal\n>Group type: Group\n>Bot version: 1\n>Bot language: english\n»Apple-Bot"
+  local text = "⚙Group settings:⚙\n>Lock group name : "..settings.lock_name.."\n>Lock group photo : "..settings.lock_photo.."\n>Lock group member : "..settings.lock_member.."\n>Lock group leave : "..leave_ban.."\n>flood sensitivity : "..NUM_MSG_MAX.."\n>Bot protection : "..bots_protection.."\n>Public: "..public.."\nApple-Bot"
   return text
 end
 
